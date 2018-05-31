ff2035ac8880f6dc5755fb697ef21ac45e4e4cb2
Shaun Bouckaert <shaun@train-meditations.com>
2018-05-31 20:24:24 +1000

    H2C server skeleton implementation with hkwi/h2c

--- muacs.go
+++ muacs.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-//	"golang.org/x/net/http2"
-//	"net"
-//	"log"
+
+	"github.com/hkwi/h2c"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -16,29 +16,9 @@ func main() {
 	listenAddress := "localhost:8081"
 	m := http.NewServeMux()
 	m.HandleFunc("/", handler)
-	s := &http.Server {
-		Addr: listenAddress,
-		Handler: m,
+	s := &http.Server{
+		Addr:    listenAddress,
+		Handler: &h2c.Server{Handler: m, DisableDirect: false},
 	}
-
-	//for http2 without tls
-	/*s2 := &http2.Server {}
-	listen, err := net.Listen("tcp", listenAddress)
-	if err != nil {
-		log.Fatal(err)//TODO Handle
-	}
-	for {
-		conn, err := listen.Accept()
-		if err != nil {
-			//TODO Handle
-			log.Print(err)
-		} else {
-			//s is a net.http.Server and m is a net.http.ServeMux
-			//TODO: check for http1.1 upgrade
-			go s2.ServeConn(conn, &http2.ServeConnOpts{BaseConfig: s, Handler: m})
-		}
-	}*/
-	
-	//for http1.1
-	s.ListenAndServe()
+	log.Fatal(s.ListenAndServe())
 }
